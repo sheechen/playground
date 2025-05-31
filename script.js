ab726fd06f0afb77429531b36575ef8f17c53784
sheechen <debug.sheechen@gmail.com>
2025-05-31 11:31:43 +0800

    Debug current question, add answered correct/wrong pages

--- script.js
+++ script.js
@@ -1,108 +1,133 @@
 let UI = [
-    {
-      question: "How old are you?",
-      option: ["10","11","12","13"],
-      answer: 3,
-      skip: 0,
-    },
-    {
-      question: "Have you eaten?",
-      option: ["yes", "no", "later", "maybe"],
-      answer: 3,
-      skip: 0,
-    },
-    {
-      question: "Are you a boy?",
-      option: ["yes", "no", "not sure", "maybe"],
-      answer: 3,
-      skip: 0,
-    },
-    {
-      question: "What is your heigth?",
-      option: ["10","11","12","13"],
-      answer: 3,
-      skip: 0,
-    },
-    {
-      question: "Do you like coding?",
-      option: ["yes", "no", "later", "maybe"],
-      answer: 3,
-      skip: 0,
-    },
-    {
-      question: "Is Debug Lab nice?",
-      option: ["yes", "no", "later", "maybe"],
-      answer: 3,
-      skip: 0,
-    },
-    {
-      question: "How long is your hair?",
-      option: ["10","11","12","13"],
-      answer: 3,
-      skip: 0,
-    },
-    {
-      question: "Can you jump?",
-      option: ["yes", "no", "later", "maybe"],
-      answer: 3,
-      skip: 0,
-    },
-    {
-      question: "Have you eaten?",
-      option: ["yes", "no", "later", "maybe"],
-      answer: 3,
-      skip: 0,
-    },
-    {
-      question: "Have you eaten?",
-      option: ["yes", "no", "later", "maybe"],
-      answer: 3,
-      skip: 0,
-    }
+  {
+    numQues: 1,
+    question: "What is the correct syntax to declare a JavaScript variable?",
+    options: ["var = myVar;", "variable myVar;", "var myVar;", "v myVar =;"],
+    answer: 3,
+  },
+  {
+    numQues: 2,
+    question: "Which company developed JavaScript?",
+    options: ["Microsoft", "Sun Microsystems", "Netscape", "Oracle"],
+    answer: 3,
+  },
+  {
+    numQues: 3,
+    question: "What will typeof null return in JavaScript?",
+    options: ["\"null\"", "\"object\"", "\"undefined\"", "\"boolean\""],
+    answer: 2,
+  },
+  {
+    numQues: 4,
+    question: "What is the result of 2 + \"2\" in JavaScript?",
+    options: ["4", "\"22\"", "NaN", "undefined"],
+    answer: 2,
+  },
+  {
+    numQues: 5,
+    question: "How do you write a comment in JavaScript?",
+    options: ["<-- comment -->", "# comment", "// comment", "** comment **"],
+    answer: 3,
+  },
+  {
+    numQues: 6,
+    question: "Which of the following is NOT a JavaScript data type?",
+    options: ["Boolean", "Undefined", "Float", "Object"],
+    answer: 3,
+  },
+  {
+    numQues: 7,
+    question: "What will the following code output? console.log(0 == '0');",
+    options: ["true", "false", "undefined", "NaN"],
+    answer: 1,
+  },
+  {
+    numQues: 8,
+    question: "Which keyword is used to define a constant in JavaScript?",
+    options: ["const", "constant", "define", "let"],
+    answer: 1,
+  },
+  {
+    numQues: 9,
+    question: "What does the Array.prototype.map() method do?",
+    options: [
+      "Modifies the original array",
+      "Filters elements based on a condition",
+      "Creates a new array with results of a function on every element",
+      "Joins array elements into a string"
+    ],
+    answer: 3,
+  },
+  {
+    numQues: 10,
+    question: "How do you check if a variable x is an array?",
+    options: [
+      "x.type === \"array\"",
+      "x instanceof Array",
+      "typeof x === \"array\"",
+      "isArray(x)"
+    ],
+    answer: 2,
+  }
 ];
+
 let currentQuestion = 0;
 let currentPoints = 0;
 let totalQues = UI.length;
-let numQues = 0;
 
 $(document).ready(function() {
   refresh();
 });
 
 function refresh(){
+  $(".next").hide();
+  $(".options-grid").show();
   //points
   var $points = $("#points");
   $points.html("Points: " + currentPoints);
   //num question
   var $numQues = $("#numQues");
-  $numQues.html("Question Answered: " + numQues + " / " + totalQues);
+  $numQues.html("Current Question: " + UI[currentQuestion]["numQues"] + " / " + totalQues);
   //question
-  var $question = $("#question");
+  var $question = $(".question");
   $question.html(UI[currentQuestion]["question"]);
   $(".option").each(function(index){
-    $(this).html(UI[currentQuestion]["option"][index]);
+    $(this).html(UI[currentQuestion]["options"][index]);
   });
 }
 
 function answer(i) {
   if (i == UI[currentQuestion]["answer"]) {
-    alert("Correct answer.")
-    currentPoints++;
-    nextQuestion();
+    correct();
   } else {
-    alert("Wrong answer.");
-    nextQuestion();
+    wrong();
   }
 }
 
+function wrong() {
+  $(".options-grid").hide();
+  $(".question").html("Wrong ! Answer is " + UI[currentQuestion]["options"][UI[currentQuestion]["answer"]-1]);
+  $(".next").show();
+}
+
+function correct() {
+  $(".options-grid").hide();
+  $(".question").html("Correct !");
+  currentPoints++;
+  $(".next").show();
+}
+
 function nextQuestion() {
-  if (currentQuestion < UI.length){
-    numQues++;
+  if (UI[currentQuestion]["numQues"] != totalQues) {
     currentQuestion++;
     refresh();
+  } else {
+    finished();
   }
-  else{
-      $(".options-grid").hide();
-      $("#question").html("You get 10 points for this");
-  }
+}
+
+function finished() {
+  $(".options-grid").hide();
+  $(".next").hide();
+  $(".question").html("You get " + currentPoints + " points for this Quiz");
 }
